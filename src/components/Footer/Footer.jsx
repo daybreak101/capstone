8efa1cb3c67d3e19c4f92931cf9a13a87b79b92b
Ramiro F <ramirofernandez244@gmail.com>
2026-08-26 20:11:37 -0700

    Started Semantic HTML refresher

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        Doormat Navigation
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div>
+        Contact
+        <ul>
+          <li>Address</li>
+          <li>Phone Number</li>
+          <li>Email</li>
+        </ul>
+      </div>
+      <div>
+        Social Media Links
+        <ul>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>X</li>
+          <li>TikTok</li>
+          <li>Pinterest</li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+export default function Header() {
+  return (
+    <header>
+      <img src="images/Logo.svg" alt="Header Logo" />
+      <Nav />
+    </header>
+  );
+}
