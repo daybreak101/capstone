5dff1cb2d6d699826d4579784ac77cab25e2f735
Ramiro F <ramirofernandez244@gmail.com>
2026-08-30 14:40:53 -0700

    Began Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
-import HomePage from './pages/HomePage/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import BookingPage from "./pages/BookingPage/BookingPage";
 
 function App() {
   return (
-      <HomePage />
+    // <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/booking" element={<BookingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
